3ebabf4d25cbae890e548d5aeede71de5d6cec7d
Allan Holmes <allan.holmes@me.com>
2019-05-08 20:04:40 +0200

    save before I start working on the how-to content

---
+++ animate.js
@@ -0,0 +1,12 @@
+
+
+setTimeout(function(){
+  document.getElementById('play-button').style.visibility = "visible";
+  document.getElementById('play-button').style.transition = "ease";
+},3000);
+
+setTimeout(function(){
+document.getElementById('howto').style.visibility = "visible";
+},3500);
+
+
